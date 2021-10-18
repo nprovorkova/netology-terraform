a3a6ba2b202a393c1c2e1f02b90e8799f7f36604
Nataliya Provorkova <provorkova@mail.ru>
2021-10-18 21:55:57 +0300

    terraform base

---
+++ main.tf
@@ -0,0 +1,45 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-north-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "netology-state-dev"
+    key    = "bucket-key-path"
+    region = "eu-north-1"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+     filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
+locals {
+  web_instance_type_map = {
+    stage = "t3.micro"
+    prod = "t3.large"
+  }
+}
+
+resource "aws_instance" "aws_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  count         = 1
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
